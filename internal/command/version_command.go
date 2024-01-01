f1940910eeb78609c89a65f61ad418f4322fa42d
Egon Braun <egon@mundoalem.io>
2024-01-01 21:15:08 +0000

    Initial

---
+++ internal/command/version_command.go
@@ -0,0 +1,53 @@
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// LICENSE
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//
+// This file is part of pokas.
+//
+// The pokas is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+// General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// The pokas is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Affero General Public License along with pokas. If not, see
+// <https://www.gnu.org/licenses/>.
+//
+//
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+package command
+
+import (
+	"fmt"
+)
+
+// Version command implements the interface cli.Command. It outputs to the console metadata about
+// the program.
+type VersionCommand struct {
+	Commit    string
+	BuildTime string
+	Version   string
+}
+
+// Help returns a string describing the Version command in more details to the user
+func (c *VersionCommand) Help() string {
+	return "Shows the command version and build metadata"
+}
+
+// Run is the core function of the Version command which runs the requested process
+func (c *VersionCommand) Run(args []string) int {
+	fmt.Println("Version:    " + c.Version)
+	fmt.Println("Build Time: " + c.BuildTime)
+	fmt.Println("Commit:     " + c.Commit)
+
+	return 0
+}
+
+// Synopsys returns a string containing a short message about the Version command
+func (c *VersionCommand) Synopsis() string {
+	return "Shows the command version and other related information"
+}
